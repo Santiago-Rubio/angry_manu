ce7f700aed03d63e0d7d37230901917c79e9b4a6
EvoRaptar <santiagopalou@gmail.com>
2025-01-17 01:22:38 +0100

    Clase donde se ira generando las frutas que tendremos que ir destruyendo para que se nos vaya sumando la puntuacion

--- lib/Games/AngryManuGame.dart
+++ lib/Games/AngryManuGame.dart
@@ -5,12 +5,68 @@ import 'dart:async';
 import 'package:flame/components.dart';
 import 'package:flame/events.dart';
 import 'package:flame/game.dart';
+import 'package:flutter/cupertino.dart';
 
-class AngryManuGame extends FlameGame with HasKeyboardHandlerComponents, HasCollisionDetection{
+class AngryManuGame extends Forge2D with HasKeyboardHandlerComponents, HasCollisionDetection{
 
 
   @override
   bool get debugMode => true;
+  late Background background;
+  late Scoreboard scoreboard;
+  late double screenHeight;
+  late double screenWidth;
+  late List<Fruit> fruits;
 
-  
+  @override
+  Future<void> onLoad() async {
+    screenHeight = size.y;
+    screenWidth = size.x;
+
+    background = Background();
+    add(background);
+
+    scoreboard = Scoreboard();
+    add(scoreboard);
+
+    fruits = [];
+    spawnFruit();
+  }
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+
+    // Actualizar cada fruta en la lista
+    for (var fruit in fruits) {
+      fruit.update(dt);
+    }
+  }
+
+  @override
+  void onPanUpdate(DragUpdateDetails details) {
+    super.onPanUpdate(details);
+
+    // Cortar las frutas al tocar la pantalla
+    for (var fruit in fruits) {
+      if (fruit.toRect().contains(details.localPosition)) {
+        fruit.cut();
+        scoreboard.incrementScore(); // Sumar puntos al cortar una fruta
+        remove(fruit);
+        spawnFruit(); // Generar una nueva fruta
+      }
+    }
+  }
+
+  void spawnFruit() {
+    // Generar frutas al azar en la parte inferior de la pantalla
+    final fruit = Fruit(world)
+      ..position = Vector2(screenWidth * 0.5, screenHeight)
+      ..size = Vector2(50, 50)
+      ..speed = 300;
+    fruits.add(fruit);
+    add(fruit);
+  }
+}
+
 }
